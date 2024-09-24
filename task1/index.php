ae60ed0a2f6e7e907d45e28ff4d7c668b1fda72a
bottik4 <117823193+bottik4@users.noreply.github.com>
2024-09-24 16:41:57 +0300

    Add files via upload

---
+++ task1/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+</head>
+<body>
+    <form action="" method="get">
+        <input type="date" name="date1" required>
+        <input type="date" name="date2" required>
+        <input type="submit">
+    </form>
+    <?php
+    if (isset($_GET["date1"])) {
+        $date1 = strtotime($_GET["date1"]);
+        $date2 = strtotime($_GET["date2"]);
+        $second = abs($date1 - $date2);
+        $year = floor($second / 31536000);
+        $day = floor($second / 86400);
+        $hour = $day * 24;
+        $minute = $hour * 60; 
+    ?>
+        <h3>Разница дат (<? echo $_GET["date1"] ?> — <? echo $_GET["date2"] ?>):</h3>
+        <h4>В годах: <? echo $year ?></h4>
+        <h4>В днях: <? echo $day ?></h4>
+        <h4>В часах: <? echo $hour ?></h4>
+        <h4>В минутах: <? echo $minute ?></h4>
+        <h4>В секундах: <? echo $second ?></h4>
+    <? } ?>
+</body>
+</html>
