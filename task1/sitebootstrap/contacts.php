c074562c40e10db53df8c3ff6939cbf93e8ec0d7
bottik4 <117823193+bottik4@users.noreply.github.com>
2024-12-10 10:42:51 +0300

    Add files via upload

---
+++ task1/sitebootstrap/contacts.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./bootstrap.min.css">
+    <script src="./bootstrap.bundle.min.js" defer></script>
+    <title>Контакты</title>
+</head>
+  <header class="p-3">
+      <nav class="navbar justify-content-center navbar-expand-lg fs-5">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" href="index.php">Главная</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="info.php">Наши проекты</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="history.php">История</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="news.php">Новости</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="for_clients.php">Для клиентов</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="contacts.php">Контакты</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="about_us.php">О нас</a>
+        </li>
+      </ul> 
+    </nav>  
+  </header>
+<body class="p-3">
+    <main class="container">
+        <h1>Контакты</h1>
+        <ul class="list-group fs-5">
+            <li class="list-group-item">Телефон: <a href="tel:+78005553535">+7 (800) 555 35 35</a></li>
+            <li class="list-group-item">Электронная почта: <a href="mailto:contact@web.ru">contact@graphstudio.ru</a></li>
+            <li class="list-group-item">Адрес: <a href="https://yandex.ru/maps/org/moskva_siti/1192932552/?ll=37.540858%2C55.746561&source=serp_navig&z=15.16" target="_blank">Москва, Москва-Сити</a></li>
+        </ul>
+    </main>
+</body>
+</html>
