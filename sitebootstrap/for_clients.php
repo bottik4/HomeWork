e0908cbb60d5c33c876c27db217bc4c9ad0c0610
bottik4 <117823193+bottik4@users.noreply.github.com>
2024-12-10 10:44:07 +0300

    Add files via upload

---
+++ sitebootstrap/for_clients.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./bootstrap.min.css">
+    <script src="./bootstrap.bundle.min.js" defer></script>
+    <title>Для клиентов</title>
+</head>
+<body class="p-3">
+  <header class="p-3">
+      <nav class="navbar justify-content-center navbar-expand-lg fs-5">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" href="index.php">Главная</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="info.php">Наши проекты</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="history.php">История</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="news.php">Новости</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="for_clients.php">Для клиентов</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="contacts.php">Контакты</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="about_us.php">О нас</a>
+        </li>
+      </ul> 
+    </nav>  
+  </header>
+    <main class="container">
+        <h1>Для клиентов</h1>
+        <div class="d-flex flex-column fw-bold fs-5">
+            <span>Сколько стоит курс?</span>
+            <span>Как долго проходит обучение?</span>
+            <span>Будет ли сертификат?</span>
+            <span>Все ответы здесь -> <a class="fs-5" href="contacts.php">Контакты</a></span>
+        </div>
+    </main>
+</body>
+</html>
---
+++ sitebootstrap/news.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./bootstrap.min.css">
+    <script src="./bootstrap.bundle.min.js" defer></script>
+    <title>Новости</title>
+</head>
+<body class="p-3">
+  <header class="p-3">
+      <nav class="navbar justify-content-center navbar-expand-lg fs-5">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" href="index.php">Главная</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="info.php">Наши проекты</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="history.php">История</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="news.php">Новости</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="for_clients.php">Для клиентов</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="contacts.php">Контакты</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="about_us.php">О нас</a>
+        </li>
+      </ul> 
+    </nav>  
+  </header>
+    <main class="container">
+        <h1 class="mb-5">Новости</h1>
+        <div class="list-group">
+            <div class="d-flex w-100 justify-content-between">
+                <h4>Набор в группу</h4>
+                <p>23.10.2024</p>
+            </div>
+            <p class="mb-5">Идет набор в группу, по всем вопросам обращаться по телефону.</p>
+
+            <div class="d-flex w-100 justify-content-between">
+                <h4>Смена цен на курсы</h4>
+                <p>20.10.2024</pз>
+            </div>
+            <p class="mb-5">С 1 января 2025 года изменение цен на курсы по графическому дизайну. В связи с увеличением затрат на повышение уровня квалификации специалистов, стоимость базового курса вырастет на 15%.</p>
+
+            <div class="d-flex w-100 justify-content-between">
+                <h4>Создание сайтов на React</h4>
+                <p>18.10.2024</p>
+            </div>
+            <p class="mb-5">Теперь мы можетм создавать современные и высококачественные веб-сайты на основе популярной библиотеки React.</p>
+        </div>
+    </main>
+</body>
+</html>
